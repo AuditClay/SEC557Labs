00eba3d8492320cfe4afa126031434e2ce0ccced
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-11-28 13:47:13 -0600

    Create Run-GithubIssues.ps1

---
+++ Run-GithubIssues.ps1
@@ -0,0 +1,13 @@
+<#
+Run script for the github issues metric database. This script will
+be run in a scheduled task every day
+#>
+#Get the base name of the script to use as the base name of the transcript file
+$scriptName = $MyInvocation.MyCommand.Name.Replace(".ps1","")
+$logPath = "c:\users\auditor\sec557labs\$scriptName.txt"
+
+Start-Transcript -Path $logPath
+
+c:\users\auditor\sec557labs\GithubIssues.ps1
+
+Stop-Transcript
