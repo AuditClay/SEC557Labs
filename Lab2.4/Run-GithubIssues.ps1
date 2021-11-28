065232a5f1eff782dd7ccfb526a5fead017ecf57
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-11-28 17:28:03 -0600

    lab 2.4

--- Lab2.4/Run-GithubIssues.ps1
+++ Lab2.4/Run-GithubIssues.ps1
@@ -4,10 +4,7 @@ be run in a scheduled task every day
 #>
 #Get the base name of the script to use as the base name of the transcript file
 $scriptName = $MyInvocation.MyCommand.Name.Replace(".ps1","")
-$logPath = "c:\users\auditor\sec557labs\$scriptName.txt"
-
+$logPath = "c:\automation\logs\$scriptName.txt"
 Start-Transcript -Path $logPath
-
-c:\users\auditor\sec557labs\GithubIssues.ps1 -verbose
-
+c:\automation\GithubIssues.ps1 -verbose
 Stop-Transcript
