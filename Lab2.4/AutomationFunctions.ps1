db4e546a93f8f22d4acbc9b30ad23fcb852b10c6
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-11-28 18:52:46 -0600

    Update AutomationFunctions.ps1

--- Lab2.4/AutomationFunctions.ps1
+++ Lab2.4/AutomationFunctions.ps1
@@ -9,18 +9,26 @@ function Send-TCPData {
 
     #process the pipeline input
     begin {
+        $linesProcessed = 0
         Write-Verbose "Opening socket to $remoteHost`:$remotePort"
         $socket = new-object System.Net.Sockets.TcpClient($remoteHost, $remotePort)
     }
     process {
         Write-Verbose "Received $metrics"
-        $message += ($metrics + "`n")
-        
+        $linesProcessed += $metrics.Count
+        $metrics | ForEach-Object {
+            $message += ($_ + "`n")
+        }
     }
     end {
         Write-Verbose "Sending $message"
         $dataBytes = [System.Text.Encoding]::ASCII.GetBytes($message)
         $stream = $socket.GetStream()
         $stream.Write($dataBytes, 0, $dataBytes.Length)
+        Write-Verbose "$linesProcessed metrics processed"
+        Write-Verbose "Closing stream"
+        $stream.Close()
+        Write-Verbose "Closing socket"
+        $socket.Close()
     }
 }
