5f81bd6dacd0c7c9cc0f3c09852f8da2ad92a5df
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-12-04 15:04:22 -0600

    Update Solution3.1.ps1

--- Lab3.1/Solution3.1.ps1
+++ Lab3.1/Solution3.1.ps1
@@ -6,7 +6,7 @@ Set-Location C:\Users\auditor\SEC557Labs\Lab3.1\
 $patchAgeData = Import-Csv .\patchAge.csv
 
 $patchAgeData | 
-  select ServerName,PatchAge, @{n='epoch';e={Get-Date -Date $_.Date -AsUTC -UFormat %s}} | 
+  Select-Object ServerName,PatchAge, @{n='epoch';e={Get-Date -Date $_.Date -AsUTC -UFormat %s}} | 
   ForEach-Object { "patchage." + $_.ServerName + " " + $_.PatchAge + " " + $_.epoch} | 
   wsl nc -vv -N 10.50.7.50 2003
 
