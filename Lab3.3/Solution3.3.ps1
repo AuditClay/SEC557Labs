e59dfaaabab8f837adb7c853531d30e4ff1fe5e5
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-12-04 15:07:41 -0600

    Update Solution3.3.ps1

--- Lab3.3/Solution3.3.ps1
+++ Lab3.3/Solution3.3.ps1
@@ -1,6 +1,7 @@
 #Run on Win10
-Set-Location C:\SEC557\Lab3.3
-$cred = Get-Credential -UserName auditor
+Set-Location C:\Users\auditor\SEC557Labs\Lab3.3\
+$password = ConvertTo-SecureString "Password1" -AsPlainText -Force
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList("auditor",$password)
 
 .\ADDemographics.ps1 -Credential $cred -Server 10.50.7.10 | 
   wsl nc -N -vv ubuntu 2003
