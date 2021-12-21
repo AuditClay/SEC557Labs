606a4d38d2eb497c6d08fb3ff18217cd646122cf
AuditClay <42394776+AuditClay@users.noreply.github.com>
2021-12-20 19:09:59 -0600

    Capstone

--- Capstone/Generate-Capstone.ps1
+++ Capstone/Generate-Capstone.ps1
@@ -40,6 +40,34 @@ function Get-NumberedName {
 
 #Generators
 
+Function Generate-HostScanData {
+    param( $PatchAgeMinWeeks = 0)
+    $missingPatches = Get-Random -Minimum 0 -Maximum 15
+    $patchList = @()
+    for( $i = 0; $i -lt $missingPatches; ++$i){
+        $ageFactor = Get-Random -Minimum 0 -Maximum 100
+
+        #90% chance of 1-4 weeks of age
+        if( $AgeFactor -lt 95 ){
+            $weeks = Get-Random -Minimum $patchAgeMinWeeks -Maximum 4
+        }
+        elseif ($patchAge -lt 98){
+            $weeks = Get-Random -Minimum 4 -Maximum 7
+        } 
+        else {
+            $weeks = Get-Random -Minimum 7 -Maximum 10
+        }
+        $patchAge = ($weeks * 7) + 2
+        $patchNum = Get-Random -Minimum 1000000 -Maximum 9999999
+        $firstSeenDate = ((Get-Date).AddDays(0 - $patchAge )).ToShortDateString()
+        $ageEntry = [PSCustomObject]@{
+            FirstSeenDate = $firstSeenDate
+            PatchID = $patchNum
+        }
+        $patchList += $ageEntry
+     }
+     $patchList 
+}
 function Get-VulnScanData {
     param (
         [int]$MaxHosts = 100,
@@ -54,67 +82,23 @@ function Get-VulnScanData {
     for ($hostCount = 0; $hostCount -lt $MaxHosts; $hostCount++) {
         $hostName = Get-NumberedName -Name "Host" -Digit ($hostCount + 1)
 
-        $scanValue = Get-Random -Minimum 0 -Maximum 99
-
-        if ($scanValue -ge 10) {
-            $scanDateOffset = -2
-        }
-        elseif ($scanValue -eq 4) {
-            $scanDateOffset = -9
-        }
-        elseif ($scanValue -eq 0) {
-            $scanDateOffset = -16
+        $missedThisWeekFactor = Get-Random -Minimum 0 -Maximum 100
+        if($missedThisWeekFactor -lt 3) {
+            $patchAgeMinWeeks = 1
         }
         else {
-            $scanDateOffset = -99
+            $patchAgeMinWeeks = 0
         }
-        
-
-        #$baseDate = Get-Date -Hour 0 -Minute 0 -Second 0 -Millisecond 0
-        $baseDate = Get-Date -Date ((Get-Date).ToShortDateString())
-        $lastScannedDate = ($baseDate.AddDays($scanDateOffset)).ToShortDateString()
-
-
-        for ($missingPatchCount = 0; $missingPatchCount -lt $MaxMissingPatches; $missingPatchCount++) {
-
-            $patchNumber = Get-Random -Minimum 11111 -Maximum 99999999
-            $patchID = Get-NumberedName -Name "" -Digit ($patchNumber) -PadAmt 8
-
-            $foundWeek = Get-Random -Minimum 0 -Maximum $MaxWeeks
-
-            if ($foundWeek -ge 2) {
-                $foundWeekOffset = (($foundWeek * 7) + 2) * -1
-            }
-            elseif ($foundWeek -eq 1) {
-                $foundWeekOffset = (($foundWeek * 7) + 2) * -1
-            }
-            elseif ($foundWeek -eq 0) {
-                $foundWeekOffset = (($foundWeek * 7) + 2) * -1
-            }
-            else {
-                $foundWeekOffset = -99
-            }
-
-            $lastScannedDateValue = (Get-Date -Date ((Get-Date).ToShortDateString())).AddDays($scanDateOffset)
-            $baseFoundDate = Get-Date -Date (($lastScannedDateValue).ToShortDateString())
-            $firstFoundDate = ($baseFoundDate.AddDays($foundWeekOffset)).ToShortDateString()
-
-            $missingPatchEntry = [PSCustomObject] @{
-                "PatchID" = $patchID
-                "FirstSeenDate" = $firstFoundDate
-            }
-
-            $MissingPatches += $missingPatchEntry
+        $scanAge = ($patchAgeMinWeeks * 7) + 2
+        $lastScanDate = ((Get-Date).AddDays(0 - $scanAge )).ToShortDateString()
+    
+        $patchList = Generate-HostScanData -PatchAgeMinWeeks $patchAgeMinWeeks
+        $patchEntry = [PSCustomObject]@{
+            Hostname = $hostName
+            LastScanDate = $lastScanDate
+            MissingPatches = $patchList
         }
-
-
-        $scannedHostsEntry = [PSCustomObject] @{
-            "Hostname" = $hostName
-            "LastScanDate" = $lastScannedDate
-            "MissingPatches" = $MissingPatches
-        }
-
-        $ScannedHosts += $scannedHostsEntry
+        $ScannedHosts += $patchEntry
     }
 
     return $ScannedHosts
@@ -223,14 +207,14 @@ function Get-HostGrouplists {
 #Set Random
 Get-Random -SetSeed 314159 | out-null
 
+Get-HostInventory -MaxHosts 100 | ConvertTo-Csv | Out-File "hostInventory.csv"
 
-#Get-HostInventory -MaxHosts 100 | ConvertTo-Csv | Out-File "hostInventory.csv"
-
-
-#$GroupListData = Get-HostGrouplists
-#$GroupListData | ConvertTo-Json -Depth 5 -Compress | Out-File "groupList.json"
+$GroupListData = Get-HostGrouplists
+$GroupListData | ConvertTo-Json -Depth 5 -Compress | Out-File "groupList.json"
 #Solution:
 #.\Generate-Capstone.ps1 | ConvertFrom-Json | Select-Object @{Name="Hostname"; Expression={$_.Hostname}}, @{N="Admincount"; E={($_.Groups | Where-Object Groupname -eq "Administrators").Users.Count}}
 #Get-Content .\groupList.json | ConvertFrom-Json | Select-Object @{Name="Hostname"; Expression={$_.Hostname}}, @{N="Admincount"; E={($_.Groups | Where-Object Groupname -eq "Administrators").Users.Count}} | Where-Object Admincount -gt 4 | Measure-Object
 
-Get-VulnScanData -MaxHosts 100 -MaxMissingPatches 10 
+Get-VulnScanData -MaxHosts 100 -MaxMissingPatches 10 | ConvertTo-Json -Depth 5 | Out-File VulnScans.json
+
+#Get-Content .\VulnScans.json | ConvertFrom-Json | Select-Object Hostname, @{n='MaxPatchAge';e={(New-TimeSpan -Start ($_.MissingPatches | Sort-Object FirstSeenDate | Select-Object -first 1).FirstSeenDate -End (Get-Date)).TotalDays}} | Select-Object Hostname, MaxPatchAge, @{n='ZeroMax';e={if ($null -eq $_.MaxPatchAge){0} else {$_.MaxPatchAge}}}
