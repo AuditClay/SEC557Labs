fcd80169a524ada6d4b5e515e0b35cc06b2903aa
Gur <isaiahposey.is@gmail.com>
2022-05-18 13:42:49 -0500

    Update Win10.tests.ps1

--- Pester/Win10.tests.ps1
+++ Pester/Win10.tests.ps1
@@ -323,20 +323,20 @@ Describe 'Tests for Win10 VM' {
         }
 
         It 'Windows.Tests.ps1 has 7 passed tests' {
-            $pesterResult = Invoke-Pester -Path C:\users\auditor\SEC557Labs\Lab3.2\\Windows.Tests.ps1 -PassThru
+            $pesterResult = Invoke-Pester -Path C:\users\student\SEC557Labs\Lab3.2\\Windows.Tests.ps1 -PassThru
             $pesterResult.PassedCount | Should -Be 7
         }
         It 'Windows.Tests.ps1 has 2 failed tests' {
-            $pesterResult = Invoke-Pester -Path C:\users\auditor\SEC557Labs\Lab3.2\\Windows.Tests.ps1 -PassThru
+            $pesterResult = Invoke-Pester -Path C:\users\student\SEC557Labs\Lab3.2\\Windows.Tests.ps1 -PassThru
             $pesterResult.FailedCount | Should -Be 2
         }
         It 'PesterIntro.tests.ps1 has 10 passed tests' {
-            $pesterResult = Invoke-Pester -Path C:\users\auditor\SEC557Labs\Lab3.2\\pesterintro.Tests.ps1 -PassThru
+            $pesterResult = Invoke-Pester -Path C:\users\student\SEC557Labs\Lab3.2\\pesterintro.Tests.ps1 -PassThru
             $pesterResult.passedCount | Should -Be 10
             
         }
         It 'PesterIntro.tests.ps1 has 2 failed tests' {
-            $pesterResult = Invoke-Pester -Path C:\users\auditor\SEC557Labs\Lab3.2\\pesterintro.Tests.ps1 -PassThru
+            $pesterResult = Invoke-Pester -Path C:\users\student\SEC557Labs\Lab3.2\\pesterintro.Tests.ps1 -PassThru
             $pesterResult.failedCount | Should -Be 2
         }
 
